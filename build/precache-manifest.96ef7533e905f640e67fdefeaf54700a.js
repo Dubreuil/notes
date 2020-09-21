6bd2005e4824293a133e112069a019b1e22d6d44
Mathieu Dubreuil <dubm1811@usherbrooke.ca>
2020-09-21 00:00:21 -0400

    added frontend

---
+++ build/precache-manifest.96ef7533e905f640e67fdefeaf54700a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a3f628e5260d81bdb230fbe2b568e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89cd39c0c51ff86bf1f1",
+    "url": "/static/css/main.f73f7164.chunk.css"
+  },
+  {
+    "revision": "2c72523954b50e4f26c9",
+    "url": "/static/js/2.0527d671.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0527d671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89cd39c0c51ff86bf1f1",
+    "url": "/static/js/main.285307a6.chunk.js"
+  },
+  {
+    "revision": "faaae247f2136725277e",
+    "url": "/static/js/runtime-main.dd85382f.js"
+  }
+]);
---
+++ build/precache-manifest.e27aeb6afb87a5a1ed2881ddd190d63c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e599c6e0a94358ad43a11d8099687c56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f93de4731160ba07beab",
+    "url": "/static/css/main.f73f7164.chunk.css"
+  },
+  {
+    "revision": "2c72523954b50e4f26c9",
+    "url": "/static/js/2.0527d671.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0527d671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93de4731160ba07beab",
+    "url": "/static/js/main.6d4fd614.chunk.js"
+  },
+  {
+    "revision": "faaae247f2136725277e",
+    "url": "/static/js/runtime-main.dd85382f.js"
+  }
+]);
